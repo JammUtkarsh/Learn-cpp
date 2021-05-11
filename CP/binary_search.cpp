08a90e3002caf515be3c84538118d2de5092949e
JammUtkarsh <5.utkarshchourasia@gmail.com>
2021-05-11 14:07:45 +0530

    files changes made

--- CP/binary_search.cpp
+++ CP/binary_search.cpp
@@ -5,25 +5,30 @@ using namespace std;
 int main()
 {
 	int a[10]={1,2,3,4,5,6,7,8,9,10};
-	int l=0,h=9,mid,key;
+	int l = a[0],h = a[9],mid,key;
 	cout<<"Enter the Key ";
 	cin>>key;
 	
-	while(l<=h)
+	while(l <= h)
 	{
 		mid=(l+h)/2;
-		if(key==a[mid])
+		if(key == a[mid])
 		{
-			int found=mid;
+			int found = mid;
 			found++;
 			cout<<"Key found at "<<found<<" position";
 			return 0;
 		}
 		else if(key<a[mid])
-				h=mid-1;
+				h = mid-1;
 		else 
-				l=mid+1;
+				l = mid+1;
 	}
-	cout<<"Key not found ";
+	if (h < l)
+	{
+		cout<<"Key not found: ";
+
+	}
+	
 	return 0;
 }
